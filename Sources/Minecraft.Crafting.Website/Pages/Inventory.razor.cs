2a0f37e0abe6b2373d1e51744685394620abf63d
yorick geoffre <yorick.geoffre@etu.uca.fr>
2023-02-01 17:02:30 +0100

    work on inventory page

---
+++ Sources/Minecraft.Crafting.Website/Pages/Inventory.razor.cs
@@ -0,0 +1,41 @@
+﻿using Blazored.LocalStorage;
+using Blazored.Modal;
+using Blazored.Modal.Services;
+using Blazorise.DataGrid;
+using Microsoft.AspNetCore.Components;
+using Microsoft.Extensions.Localization;
+using Minecraft.Crafting.Website.Components;
+using Minecraft.Crafting.Website.Modals;
+using Minecraft.Crafting.Website.Models;
+using Minecraft.Crafting.Website.Services;
+
+namespace Minecraft.Crafting.Website.Pages
+{
+    public partial class Inventory
+    {
+        [Inject]
+        public IDataService DataService { get; set; }
+
+        [Inject]
+        public IStringLocalizer<List> Localizer { get; set; }
+
+        [ParameterAttribute]
+        public List<Item> Items { get; set; } = new List<Item>();
+
+
+        protected override async Task OnAfterRenderAsync(bool firstRender)
+        {
+            base.OnAfterRenderAsync(firstRender);
+
+            if (!firstRender)
+            {
+                return;
+            }
+
+            Items = await DataService.List(0, await DataService.Count());
+
+            StateHasChanged();
+        }
+
+    }
+}
