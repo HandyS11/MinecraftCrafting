5c06b3acae1d3218dfcc0ef06203b78083cd5c58
HandyS11 <62420910+HandyS11@users.noreply.github.com>
2023-02-26 22:43:37 +0100

    fix trad and drone

--- Sources/Minecraft.Crafting.Website/Components/Inventory.razor.cs
+++ Sources/Minecraft.Crafting.Website/Components/Inventory.razor.cs
@@ -1,42 +1,39 @@
 ﻿using Microsoft.AspNetCore.Components;
-using Microsoft.JSInterop;
 using System.Collections.ObjectModel;
-using System.Collections.Specialized;
-using Minecraft.Crafting.Website.Models;
-using Microsoft.Extensions.Logging;
-using static System.Net.WebRequestMethods;
 using Minecraft.Crafting.Website.Pages;
 using Minecraft.Crafting.Website.Services;
 using Blazored.LocalStorage;
+using Microsoft.Extensions.Localization;
 
 namespace Minecraft.Crafting.Website.Components
 {
-        public partial class Inventory
-        {
+    public partial class Inventory
+    {
+        public ObservableCollection<CraftingAction> Actions { get; set; }
 
-            public Inventory()
-            {
-            }
+        [Parameter]
+        public InventoryPage Parent { get; set; }
 
-            public ObservableCollection<CraftingAction> Actions { get; set; }
+        //[Parameter]
+        public List<Models.Item> Items { get; set; }
 
-            [Parameter]
-            public InventoryPage Parent { get; set; }
+        [Inject]
+        public IInventoryService InventoryService { get; set; }
 
-            //[Parameter]
-            public List<Website.Models.Item> Items { get; set; }
+        [Inject]
+        public ILogger<LogModel> Logger { get; set; }
 
-            [Inject]
-            public IInventoryService InventoryService { get; set; }
-            [Inject]
-            public ILogger<LogModel> Logger { get; set; }
-            [Inject]
-            public HttpClient Http { get; set; }
+        [Inject]
+        public HttpClient Http { get; set; }
 
-            [Inject]
-            public ILocalStorageService LocalStorage { get; set; }
-            [Inject]
-            public NavigationManager NavigationManager { get; set; }
-       }
+        [Inject]
+        public ILocalStorageService LocalStorage { get; set; }
+
+        [Inject]
+        public NavigationManager NavigationManager { get; set; }
+
+        [Inject]
+        public IStringLocalizer<Add> Localizer { get; set; }
+    }
 
 }
